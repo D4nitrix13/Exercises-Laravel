866fef8a73812591813752c700dd9607c9a004ad
d4nitrix13 <danielperezdev@proton.me>
2025-03-01 12:32:51 -0600

    Exercises 1 Complete

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,31 @@ Route::get('/', function () {
 
 // Ejercicio 1
 
+// Crea 5 Rutas Distintas Cuya URI Sea /Ejercicio1 Pero El Método Http Sea Cada Uno De Los Siguientes:
+//     • GET
+//     • POST
+//     • PUT
+//     • PATCH
+//     • DELETE
+
+// Cuando El Servidor Recibe Una Petición A Esa Ruta, Debe Devolver Una Respuesta En Texto Plano Con El Nombre Del Método Seguido De Ok. Por Ejemplo, La Petición Post /Ejercicio1 Debe Devolver POST OK, Y Así Sucesivamente.
+
 Route::get('/ejercicio1', function () {
     return "GET OK";
 });
 
 Route::post('/ejercicio1', function () {
     return "POST OK";
+});
+
+Route::put('/ejercicio1', function () {
+    return "PUT OK";
+});
+
+Route::patch('/ejercicio1', function () {
+    return "PATCH OK";
+});
+
+Route::delete('/ejercicio1', function () {
+    return "DELETE OK";
 });
