66402147a9652c3de428d44905fbec190fe55660
D4nitrix13 <danielperezdev@proton.me>
2025-06-16 09:22:52 -0600

    Exercises 4 Complete

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Devuelve una respuesta con los productos creados por el usuario sin mensaje. Si $products contiene una lista de modelos entonces solo debes devolver un array asociativo con este formato: ["products" => $products]
+        $products = auth()->user()->products;
+        return [
+            "products" => $products
+        ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProductRequest $request)
+    {
+        $product = auth()->user()->products()->create($request->validated());
+        return [
+            "message" => "Product created successfully",
+            "product" => $product
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return [
+            "product" => $product
+        ];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $this->authorize(ability: "update", arguments: $product);
+
+        $product->update(attributes: $request->validated());
+        return [
+            "message" => "Product updated successfully",
+            "product" => $product
+        ];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $this->authorize(ability: "delete", arguments: $product);
+
+        $product->delete();
+        return [
+            "message" => "Product deleted successfully",
+            "product" => $product
+        ];
+    }
+}
